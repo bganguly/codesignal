da234f8525e00b219dee1958b197d404e7a0ba44
bganguly <gangulybikramjit@hotmail.com>
2024-05-12 13:58:08 -0700

    updated question_10

--- 10-free-coding/answer.js
+++ 10-free-coding/answer.js
@@ -1,6 +1,12 @@
 const drawAscii = n => {
-    for (let i= 1; i<= n; i++) {
-        console.log(' '.repeat(n-i)+'*'.repeat(2*i -1))
-    }
+  // draw increasing number of *
+  for (let i= 1; i<= n; i++) {
+      console.log(' '.repeat(n-i)+'*'.repeat(2*i -1))
+  }
+  // (optional) draw decreasing number of *
+  for (let i= n; i>= 1; i--) {
+    console.log(' '.repeat(n-i)+'*'.repeat(2*i -1))
+  }
 }
+
 drawAscii(13)
