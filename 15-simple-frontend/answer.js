61fef45d30f830f7f0d759c722681de77e18021e
bganguly <gangulybikramjit@hotmail.com>
2024-05-12 18:16:11 -0700

    updated question_15

--- 15-simple-frontend/answer.js
+++ 15-simple-frontend/answer.js
@@ -5,12 +5,12 @@ const right_shift_button = document.getElementsByClassName("right-shift-button")
 const boxesHtmlArray = Array.from(boxesColl);
 const numbersArray = boxesHtmlArray.map(box => box.innerHTML);
 
-left_shift_button.onclick = function() {
+left_shift_button.onclick = () => {
   numbersArray.push(numbersArray.shift());
   boxesHtmlArray.forEach((box, ind) => box.innerHTML = numbersArray[ind]);
 }
 
-right_shift_button.onclick = function() {
+right_shift_button.onclick = () => {
   numbersArray.unshift(numbersArray.pop());
   boxesHtmlArray.forEach((box, ind) => box.innerHTML = numbersArray[ind]);
 }
